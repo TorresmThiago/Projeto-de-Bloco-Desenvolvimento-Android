e45975a9797045db42a5da776a855c0a2a6c1fe3
Torres <torresmthiago@gmail.com>
2018-10-21 23:02:24 -0300

    Criação da fragment de PowerUps / Implementação na Activity

--- app/src/main/java/edu/infnet/al/izi_quiz/MainActivity.java
+++ app/src/main/java/edu/infnet/al/izi_quiz/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.infnet.al.izi_quiz;
 
 import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -31,6 +32,11 @@ public class MainActivity extends FragmentActivity {
 
     public void goToMenuOpcoes(View view) {
         replaceFragment(menuOpcoes);
+    }
+
+    public void startPartida(View view) {
+        Intent intent = new Intent(this, PartidaActivity.class);
+        startActivity(intent);
     }
 
     private void replaceFragment(Fragment fragment) {
--- app/src/main/java/edu/infnet/al/izi_quiz/PartidaActivity.java
+++ app/src/main/java/edu/infnet/al/izi_quiz/PartidaActivity.java
@@ -1,7 +1,39 @@
 package edu.infnet.al.izi_quiz;
 
-public class PartidaActivity {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
+public class PartidaActivity extends FragmentActivity {
 
+    Fragment pergunta = new PartidaPergunta();
+    Fragment powerUp = new PartidaPowerUp();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partida);
+
+        replaceFragment(powerUp);
+    }
+
+    public void goToPergunta(View view) {
+        replaceFragment(pergunta);
+    }
+
+//    public void goToPowerUp(View view) {
+//        replaceFragment(powerUp);
+//    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.partidaFragments, fragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
 }
